7af792c2e3d9a9ce972a067914f8e59dfe6ce270
cuiwm <xcuiwm@hotmail.com>
2018-01-05 11:07:02 +0800

    添加安装.vimrc 文件到 ${HOME}目录

---
+++ install_vimrc.sh
@@ -0,0 +1,16 @@
+#########################################################################
+# File Name: install.sh
+# Author: andrew Choe
+# mail: xgod@163.com
+# Created Time: Fri 05 Jan 2018 10:41:57 AM CST
+#########################################################################
+#!/bin/bash
+suffix=`date +%Y%m%d-%H%M%S`
+bakfl=.vimrc.bak.${suffix}
+
+if [ -f ${HOME}/.vimrc ] ; then
+	echo "backup ${HOME}/.vimrc to ${HOME}/"${bakfl}
+	cp -v ${HOME}/.vimrc ${HOME}/${bakfl}
+fi
+
+cp -v .vimrc  ${HOME}/.vimrc
